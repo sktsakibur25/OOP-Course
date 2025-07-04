512e2afe28d3ea4c0b03153ebbf2070cc95dfc15
Sakibur Rahman <skt.sakibur.rahman25@gmail.com>
2025-07-05 01:26:13 +0600

    Create Assignment.java

---
+++ Day_5/Practice_7/Assignment.java
@@ -0,0 +1,16 @@
+package Day_5.Practice_7;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class Assignment {
+    private String name;
+    private LocalDate dueDate;
+    private List<String> listOfTasks;
+    private List<Object> readingMaterials;
+    private List<String> listOfStudents;
+    private List<Double> marks;
+    private List<Boolean> submissionStatus;
+    
+    
+}
