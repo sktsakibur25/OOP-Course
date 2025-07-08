b4ebb43e4f60be34721d3e6c1c6b24758007c6a4
Sakibur Rahman <skt.sakibur.rahman25@gmail.com>
2025-07-08 15:57:52 +0600

    Problem_9 Completed

---
+++ Day_6/BankProblem/BankProgramMain.java
@@ -0,0 +1,35 @@
+package Day_6.BankProblem;
+
+import java.util.UUID;
+
+public class BankProgramMain {
+    public static void main(String[] args) {
+        try {
+            BankAccount account_1 = new BankAccount("Sakibur Rahman", UUID.randomUUID().toString());
+            BankAccount account_2 = new BankAccount("Tanvir Rahman", UUID.randomUUID().toString());
+
+            String message = account_1.deposit(10000.00);
+            System.out.println(message);
+
+            message = account_1.withdraw(5000.0);
+            System.out.println(message);
+
+            message = account_1.transferFund(account_2, 2000.0);
+            System.out.println(message);
+
+            message = account_2.deposit(3000.0);
+            System.out.println(message);
+
+            message = account_2.withdraw(1000.0);
+            System.out.println(message);
+
+            message = account_2.transferFund(account_1, 1000.0);
+            System.out.println(message);
+
+            System.out.println("Bank Balance: " + BankAccount.getBankBalance());
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
---
+++ Day_6/BankProblem/Exceptions/InvalidAmountException.java
@@ -0,0 +1,7 @@
+package Day_6.BankProblem.Exceptions;
+
+public class InvalidAmountException extends Exception {
+    public InvalidAmountException(String message){
+        super(message);
+    }
+}
