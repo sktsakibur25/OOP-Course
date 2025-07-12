afa68932d3d46a7e281a70c8548820e511482fd7
Sakibur Rahman <skt.sakibur.rahman25@gmail.com>
2025-07-13 02:33:32 +0600

    Update Patient.java

--- Day_8/Problem_12/Patient.java
+++ Day_8/Problem_12/Patient.java
@@ -1,5 +1,13 @@
 package Day_8.Problem_12;
 
+import java.time.LocalDate;
+
 public class Patient {
+    private String name;
+    private LocalDate dateOfBirth;
+    private String contactNumber;
+    private Doctor primaryDoctor;
+    
+
     
 }
